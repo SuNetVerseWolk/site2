0bfc7badf618e265c56dfeb81be45ac0128a9a55
Kirill <kirillbanhenco@gmail.com>
2024-05-25 21:04:06 +0300

    changes

--- front/src/layouts/Header.jsx
+++ front/src/layouts/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import styles from "styles/header.module.css";
 
 function Header() {
   return (
-    <div>
-        <h1>Домашняя страница</h1>
+    <div className={styles.container}>
+        <div>
+          <img src="logo.png" alt="..." />
+
+          <ul>
+            <li>Главная</li>
+            <li>Номера</li>
+            <li></li>
+            <li></li>
+            <li>Контакты</li>
+          </ul>
+        </div>
     </div>
   )
 }
