58066ef486d6866f1fd02889bcc82e425fdf32ac
Kirill <kirillbanhenco@gmail.com>
2024-05-26 22:55:16 +0300

    add the function of the popup

--- front/src/layouts/Header.jsx
+++ front/src/layouts/Header.jsx
@@ -1,21 +1,25 @@
 // import BurgerMenu from 'components/BurgerMenu';
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "styles/header.module.css";
 
 function Header() {
+  const [open, setIsOpened] = useState(false);
+
   return (
     <header>
-      <div className={styles.popup}>
-        <button>Войти</button>
+      {open && (
+        <div className={styles.popup}>
+          <button>Войти</button>
 
-        <ul>
-          <li><a href="">Главная</a></li>
-          <li><a href="">О нас</a></li>
-          <li><a href="">Номера</a></li>
-          <li><a href="">Услуги</a></li>
-          <li><a href="">Контакты</a></li>
-        </ul>
-      </div>
+          <ul>
+            <li><a href="">Главная</a></li>
+            <li><a href="">О нас</a></li>
+            <li><a href="">Номера</a></li>
+            <li><a href="">Услуги</a></li>
+            <li><a href="">Контакты</a></li>
+          </ul>
+        </div>
+      )}
       <div className={styles.container}>
         <a href=""><img src="logo2.png" alt="..." /></a>
 
@@ -29,7 +33,7 @@ function Header() {
 
         <button>Войти</button>
         
-        <button className={styles.burgerContainer}>
+        <button onClick={e => open ? setIsOpened(false) : setIsOpened(true)} className={styles.burgerContainer}>
           <div className={styles.burgerMenu}></div>
         </button>
       </div>
