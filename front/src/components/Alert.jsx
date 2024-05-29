f8ec57c699428218335d03764e424eebd5efddae
SuNetVerseWolk <150458547+SuNetVerseWolk@users.noreply.github.com>
2024-05-29 14:56:21 +0300

    alert by center

--- front/src/components/Alert.jsx
+++ front/src/components/Alert.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { alert } from 'styles/alert.module.css'
 
 const Alert = ({children}) => {
   return (
-    <div>
+    <div className={alert}>
       <h3>
         {children}
       </h3>
