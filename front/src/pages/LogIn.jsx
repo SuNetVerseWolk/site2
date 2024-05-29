3996830ef18fec7888e534d829425ac5928064bf
SuNetVerseWolk <150458547+SuNetVerseWolk@users.noreply.github.com>
2024-05-29 09:27:04 +0300

    login

--- front/src/pages/LogIn.jsx
+++ front/src/pages/LogIn.jsx
@@ -1,8 +1,25 @@
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import styles from 'styles/forms.module.css'
 
 const LogIn = () => {
+  const navigate = useNavigate();
+  const { mutate } = useMutation({
+    mutationFn: data => axios.post(`/api/users/logIn`, data),
+    onSuccess: res => {
+      localStorage.setItem('id', res.data.id);
+      navigate('..');
+    }
+  })
+
+  const submit = e => {
+    e.preventDefault();
+
+    mutate(Object.fromEntries(new FormData(e.target).entries()));
+  }
+
   return (
     <div className={styles.containerForm}>
        <div>
@@ -11,10 +28,10 @@ const LogIn = () => {
 
           <h2>Авторизация</h2>
 
-          <form>
-            <label htmlFor="name">
+          <form onSubmit={submit}>
+            <label htmlFor="login">
               Логин:
-              <input id='name' name='name' type="text" required />
+              <input id='login' name='login' type="text" required />
             </label>
             <label htmlFor="password">
               Пароль:
